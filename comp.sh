17d473fea843c933bc8d759be98ea0ce378c9450
H3ylc <clyeh@pm.me>
2021-10-19 06:07:13 +0300

    oops

--- comp.sh
+++ comp.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-DEB=" "
+GUBED=" "
 
-if [[ $1 -eq "D" ]]; then
-	DEB="-D DEBUG"
+if [[ $1 == "DEBUG" ]]; then
+	GUBED="-D DEBUG"
 fi
 
-gcc -Wall -g -c $DEB snake.c
-gcc -Wall -g -c $DEB mmenu.c
-gcc -Wall -g -c $DEB main.c
+gcc -Wall -g -c $GUBED snake.c
+gcc -Wall -g -c $GUBED mmenu.c
+gcc -Wall -g -c $GUBED main.c
 gcc -lncurses snake.o main.o mmenu.o -o snake
